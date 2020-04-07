9d927b7d8f960c2ad54d4d935abf399d2b02554a
Lovro Vrček <vrcek.lovro@gmail.com>
2020-04-07 18:07:04 +0800

    Added plotting training accuracy, pretrained resnet model

--- train.py
+++ train.py
@@ -1,5 +1,3 @@
-import os
-import shutil
 from tqdm import tqdm
 from time import time
 import numpy as np
@@ -10,55 +8,69 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 
 import model
-from CoverageDataset import CoverageDataset
+from pileogram import PileogramDataset
 import visualizer
 
-NORMAL = "./subsampled/normal"
-REPEATS = "./subsampled/repeats"
-CHIMERIC = "./subsampled/chimeric"
-# NON_CHIMERIC = "./original_data/non-chimeric"
+REGULAR = "./data/regular"
+REPEATS = "./data/repetitive"
+CHIMERIC = "./data/chimeric"
+JUNK = "./data/junk"
 
-EPOCHS = 15
-BATCH = 4
-PARAM_PATH = 'models/params.pt'
+EPOCHS = 10
+BATCH = 8
+PARAM_PATH = 'models/params_res18.pt'
 
 types = {
     0: 'RP',
     1: 'CH',
-    2: 'NM',
+    2: 'RG',
+    3: 'JK',
 }
+
+
+def print_confusion(conf_rep, conf_chim, conf_norm, conf_junk):
+    print("%42s" % ('Predicted'))
+    print(" " * 21 + "_" * 33)
+    print(" " * 20 + "|%10s|%10s|%10s|%10s|" % ('Repeats', 'Chimeric', 'Normal', "Junk"))
+    print(" " * 9 + "|" + "%10s" % ('Repeats') + "|%10d|%10d|%10d|%10d|"
+          % (conf_rep[0], conf_rep[1], conf_rep[2], conf_rep[3]))
+    print("True" + " " * 5 + "|" + "%10s" % ('Chimeric') + "|%10d|%10d|%10d|%10d|"
+          % (conf_chim[0], conf_chim[1], conf_chim[2], conf_chim[3]))
+    print(" " * 9 + "|" + "%10s" % ('Normal') + "|%10d|%10d|%10d|%10d|"
+          % (conf_norm[0], conf_norm[1], conf_norm[2], conf_norm[3]))
+    print(" " * 9 + "|" + "%10s" % ('Junk') + "|%10d|%10d|%10d|%10d|"
+          % (conf_junk[0], conf_junk[1], conf_junk[2], conf_junk[3]))
+
 
 def main():
     start_time = time()
     torch.manual_seed(0)
     np.random.seed(0)
-    mode = 'eval'
-
-    #    if not os.path.isdir(NON_CHIMERIC):
-    #        os.mkdir(NON_CHIMERIC)
-    #        for file in os.listdir(REGULAR):
-    #            shutil.copy(os.path.join(REGULAR, file), os.path.join(NON_CHIMERIC, file))
-    #        for file in os.listdir(REPEAT):
-    #            shutil.copy(os.path.join(REPEAT, file), os.path.join(NON_CHIMERIC, file))
+    mode = 'train'
+    #############
+    # mode = 'test'
 
     transform = transforms.Compose([
         transforms.Grayscale(),
         transforms.Resize([224, 224]),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5])
     ])
 
-    ds = CoverageDataset(REPEATS, CHIMERIC, NORMAL, transform=transform)
+    ds = PileogramDataset(REPEATS, CHIMERIC, REGULAR, JUNK, transform=transform)
     num_samples = len(ds)
     val_size = test_size = round(num_samples * 0.2)
     train_size = num_samples - val_size - test_size
     ds_train, ds_val, ds_test = random_split(ds, [train_size, val_size, test_size])
-    dl_train = DataLoader(ds_train, batch_size=BATCH, shuffle=True, num_workers=2)
-    dl_val = DataLoader(ds_val, batch_size=BATCH, shuffle=False, num_workers=2)
-    dl_test = DataLoader(ds_test, batch_size=1, shuffle=False, num_workers=2)
+    dl_train = DataLoader(ds_train, batch_size=BATCH, shuffle=True, num_workers=2, pin_memory=True)
+    dl_val = DataLoader(ds_val, batch_size=BATCH, shuffle=False, num_workers=2, pin_memory=True)
+    dl_test = DataLoader(ds_test, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
 
-    net = model.AlexNet(num_classes=3)
-    # device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # Use cuda if possible
-    device = torch.device('cpu')  # Force using cpu
+    net = model.ResNet18(num_classes=4)
+    # if device.type == 'cuda' and torch.cuda.device_count() > 1:
+    #     net = nn.DataParallel(net)
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')  # Use cuda if possible
+    # device = torch.device('cpu')  # Force using cpu
     print(f"Using device: {device}")
     net.to(device)
     criterion = nn.CrossEntropyLoss()
@@ -67,61 +79,71 @@ def main():
     optimizer = optim.RMSprop(net.parameters(), lr=1e-4)
     history_train = []
     history_val = []
-    history_acc = []
+    acc_train = []
+    acc_valid = []
 
     if mode == 'train':
         for epoch in range(EPOCHS):
-            running_loss = 0.0
             total_loss = 0.0
-            iter = 0
-            for data in tqdm(dl_train, desc=f"Epoch {epoch + 1}"):
-                net.train()
-                iter += 1
-                inputs = data['image'].to(device)
-                labels = data['label'].to(device)
+            iteration = 0
+            total = 0
+            correct = 0
+            net.train()
+
+            for data in tqdm(dl_train, desc=f"Epoch {epoch + 1}", leave=True, ncols=100):
+                iteration += 1
+                inputs = data['image'].to(device, non_blocking=True)
+                labels = data['label'].to(device, non_blocking=True)
                 optimizer.zero_grad()
                 outputs = net(inputs)
                 loss = criterion(outputs, labels)
                 loss.backward()
                 optimizer.step()
-                running_loss += loss.item()
+                # running_loss += loss.item()
                 total_loss += loss.item()
+                total += labels.size(0)
+                _, predicted = torch.max(outputs.data, 1)
+                correct += (predicted == labels).sum().item()
 
             # if i % 100 == 99:
             #    print("Epoch: %2d, Step: %5d -> Loss: %.5f" %
             #          (epoch + 1, i + 1, running_loss / 100))
             #    running_loss = 0.0
-            print(f"Epoch {epoch + 1} train loss: {total_loss / iter}")
-            history_train.append((epoch + 1, total_loss / iter))
+            accuracy = 100*correct/total
+            tqdm.write(f"Epoch {epoch + 1} train loss: {total_loss / iteration}\tAccuracy: {round(accuracy, 2)}%")
+            history_train.append((epoch + 1, total_loss / iteration))
+            acc_train.append((epoch+1, accuracy))
 
             total_loss = 0.0
-            iter = 0
+            iteration = 0
             total = 0
             correct = 0
-            for data in dl_val:
-                net.eval()
-                iter += 1
-                images = data['image'].to(device)
-                labels = data['label'].to(device)
-                outputs = net(images)
-                loss = criterion(outputs, labels)
-                total_loss += loss.item()
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+            net.eval()
+
+            with torch.no_grad():
+                for data in dl_val:
+                    iteration += 1
+                    images = data['image'].to(device)
+                    labels = data['label'].to(device)
+                    outputs = net(images)
+                    loss = criterion(outputs, labels)
+                    total_loss += loss.item()
+                    _, predicted = torch.max(outputs.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
 
             accuracy = 100 * correct / total
-            print(f"Epoch {epoch + 1}: Val loss = {total_loss / iter}, Accuracy = {accuracy}")
-            history_val.append((epoch + 1, total_loss / iter))
-            history_acc.append((epoch + 1, accuracy))
+            print(f"Epoch {epoch + 1}: Val loss = {total_loss / iteration}, Accuracy = {round(accuracy, 2)}")
+            history_val.append((epoch + 1, total_loss / iteration))
+            acc_valid.append((epoch + 1, accuracy))
 
-            if epoch == 0 or history_acc[-1] > history_acc[-2]:
+            if epoch == 0 or acc_valid[-1] > max(acc_valid[:-1]):
                 torch.save(net.state_dict(), PARAM_PATH)
 
         training_time = time()
         print(f"Finished Training. Training time: {training_time - start_time} s")
         visualizer.draw_training_curve(history_train, history_val)
-        visualizer.draw_accuracy_curve(history_acc)
+        visualizer.draw_accuracy_curve(acc_train, acc_valid)
 
     correct = 0
     total = 0
@@ -129,11 +151,14 @@ def main():
     net.eval()
     guess_repeat = []
     guess_chim = []
-    guess_normal = []
+    guess_regular = []
+    guess_junk = []
+    eval_time_start = time()
+
     with torch.no_grad(), open('wrong.txt', 'w') as f:
         for data in dl_test:
-            images = data['image'].to(device)
-            labels = data['label'].to(device)
+            images = data['image'].to(device, non_blocking=True)
+            labels = data['label'].to(device, non_blocking=True)
             paths = data['path'][0]
             # print(paths)
             # print(type(paths))
@@ -152,25 +177,31 @@ def main():
                 if predicted.item() != 1:
                     output = paths[:-4] + types[int(labels)] + '_' + types[predicted.item()] + paths[-4:] + '\n'
                     f.write(output)
-            else:
-                guess_normal.append(predicted.item())
+            elif labels == 2:
+                guess_regular.append(predicted.item())
                 if predicted.item() != 2:
                     output = paths[:-4] + types[int(labels)] + '_' + types[predicted.item()] + paths[-4:] + '\n'
                     f.write(output)
+            else:
+                guess_junk.append(predicted.item())
+                if predicted.item() != 3:
+                    output = paths[:-4] + types[int(labels)] + '_' + types[predicted.item()] + paths[-4:] + '\n'
+                    f.write(output)
 
-    evaluation_time = time()
-    print(f"Accuracy of the network on the test set: {100 * correct / total}%. ")
-    #          f"Evalutaion time: {evaluation_time - training_time} s")
-    heat_repeat = (sum([l == 0 for l in guess_repeat]), sum([l == 1 for l in guess_repeat]),
-                   sum([l == 2 for l in guess_repeat]))
-    heat_chim = (sum([l == 0 for l in guess_chim]), sum([l == 1 for l in guess_chim]),
-                 sum([l == 2 for l in guess_chim]))
-    heat_normal = (sum([l == 0 for l in guess_normal]), sum([l == 1 for l in guess_normal]),
-                   sum([l == 2 for l in guess_normal]))
+    eval_time_end = time()
+    # print(f"Accuracy of the network on the test set: {100 * correct / total}%.")
+    # print(f"Evalutaion time: {eval_time_end - eval_time_start} s.")
 
-    print(heat_repeat)
-    print(heat_chim)
-    print(heat_normal)
+    conf_repeat = (sum([l == 0 for l in guess_repeat]), sum([l == 1 for l in guess_repeat]),
+                   sum([l == 2 for l in guess_repeat]), sum([l == 3 for l in guess_repeat]))
+    conf_chim = (sum([l == 0 for l in guess_chim]), sum([l == 1 for l in guess_chim]),
+                 sum([l == 2 for l in guess_chim]), sum([l == 3 for l in guess_chim]))
+    conf_regular = (sum([l == 0 for l in guess_regular]), sum([l == 1 for l in guess_regular]),
+                   sum([l == 2 for l in guess_regular]), sum([l == 3 for l in guess_regular]))
+    conf_junk = (sum([l == 0 for l in guess_junk]), sum([l == 1 for l in guess_junk]),
+                   sum([l == 2 for l in guess_junk]), sum([l == 3 for l in guess_junk]))
+
+    print_confusion(conf_repeat, conf_chim, conf_regular, conf_junk)
 
 
 if __name__ == '__main__':
