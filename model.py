21645ae717c0e8bb86312a64049a7d5a4567690d
Lovro Vrček <vrcek.lovro@gmail.com>
2020-04-07 18:09:35 +0800

    New files added

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import torchvision.models as models
+
 
 class AlexNet(nn.Module):
 
@@ -34,7 +36,6 @@ class AlexNet(nn.Module):
             nn.Linear(4096, num_classes),
         )
 
-
     def forward(self, x):
         x = self.features(x)
         # x = self.avgpool(x)
@@ -43,11 +44,43 @@ class AlexNet(nn.Module):
         return x
 
 
-class ResNet(nn.Module):
-    def __init__(self, num_classes=2):
-        super(ResNet, self).__init__()
+class ResNet50(nn.Module):
+
+    def __init__(self, num_classes):
+        super(ResNet50, self).__init__()
         self.model = models.resnet50()
         self.model.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.fc = nn.Linear(512 * 4, num_classes)
 
     def forward(self, x):
         return self.model(x)
+
+
+class ResNet18(nn.Module):
+
+    def __init__(self, num_classes):
+        super(ResNet18, self).__init__()
+        self.model = models.resnet18(pretrained=True)
+        weight = self.model.conv1.weight.clone()
+        self.model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        with torch.no_grad():
+            self.model.conv1.weight[:, 0] = weight[:, 0]
+        self.model.fc = nn.Linear(512, 4, bias=True)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class ResNetTest(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNetTest, self).__init__()
+        original_model = models.resnet18(pretrained=True)
+        conv_in = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        self.features = nn.Sequential(
+            conv_in,
+            *list(original_model.children())[1:-1]
+        )
+        # self.fc = nn.Linear(512, 4, bias=True)
+
+    def forward(self, x):
+        return self.features(x)
--- visualizer.py
+++ visualizer.py
@@ -1,24 +1,28 @@
 from matplotlib import pyplot as plt
 
+
 def draw_training_curve(history_train, history_test):
     plt.figure()
     epochs = [h[0] for h in history_train]
     loss_train = [h[1] for h in history_train]
     loss_test = [h[1] for h in history_test]
     plt.plot(epochs, loss_train, label='train')
-    plt.plot(epochs, loss_test, label='test')
-    plt.title("Model loss")
+    plt.plot(epochs, loss_test, label='validation')
+    plt.title("Model loss over epochs")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
     plt.show()
 
-def draw_accuracy_curve(history_acc):
+
+def draw_accuracy_curve(acc_train, acc_valid):
     plt.figure()
-    epochs = [h[0] for h in history_acc]
-    acc = [h[1] for h in history_acc]
-    plt.plot(epochs, acc)
-    plt.title("Accuracy")
+    epochs = [h[0] for h in acc_train]
+    acc_train = [h[0] for h in acc_train]
+    acc_valid = [h[1] for h in acc_valid]
+    plt.plot(epochs, acc_train, label='train')
+    plt.plot(epochs, acc_valid, label='validation')
+    plt.title("Model accuracy over epochs")
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy")
     plt.show()
